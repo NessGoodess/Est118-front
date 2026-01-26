b3e2bbd2a71a9c49c4bb78e68691a17f99c0d756
Nesstor <diehere.24@gmail.com>
2026-01-26 13:53:33 -0600

    chore: configuration and tooling adjustments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ /* images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '192.168.1.125',
+        port: '8000',
+        pathname: '/private-image/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'est118.edu.mx',
+        pathname: '/private-image/**',
+      },
+    ],
+  },*/
 };
 
 export default nextConfig;
